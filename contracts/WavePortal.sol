ebb6990942e5a85efebe0f124ad3e4d418fe4e99
sebusml <seba.marquez.lutfy@gmail.com>
2021-12-22 21:04:05 -0800

    Include state in WavePortal contract and script code to execute it

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    
     constructor() {
         console.log("yo, yo, I'm a contract and I'm smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved", msg.sender); // Solidity magic
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!",  totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,3 +1,7 @@
+// Execute file with:
+// > npx hardhat run scripts/run.js
+// Hardhat will import the Hardhat Runtime Environment (hre)
+
 const main = async () => {
     // Compile and generate artifact files
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); 
@@ -8,8 +12,16 @@ const main = async () => {
     // Wait for contract to be deployed in the network, contructors are called during this process
     await waveContract.deployed();
 
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     // Get address of deployed contract
     console.log("Contract deployed to : ", waveContract.address);
+    console.log("By: ", owner.address);
+    
+    let waveCont = await waveContract.getTotalWaves();
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCont = await waveContract.getTotalWaves();
 }
 
 const runMain = async () => {
