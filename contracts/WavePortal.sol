4c1bf8a51184f243fa49e17576146b26d716d3e3
sebusml <seba.marquez.lutfy@gmail.com>
2021-12-29 18:47:41 -0800

    [Contract v3] - allow contract to send eth back to senders and update run/deploy scripts

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -17,7 +17,7 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() {
+    constructor() payable {
         console.log("Creating WavePortal v2 with <message,sender,ts> storage");
     }
 
@@ -27,6 +27,18 @@ contract WavePortal {
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        // Give prize
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+
+        // Send the sugar
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+
+        require(success, "Failed to withdraw money from contract.");
 
         console.log("%s has waved saying: %s", msg.sender, _message); 
     }
--- scripts/run.js
+++ scripts/run.js
@@ -7,10 +7,16 @@ const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); 
     
     // Hardhat creates a fresh/clean local network (blockchain) only for this contract
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     
     // Wait for contract to be deployed in the network, contructors are called during this process
     await waveContract.deployed();
+
+    // Get contract balance
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance));
 
     const [owner, randomPerson] = await hre.ethers.getSigners();
     // Get address of deployed contract
@@ -33,6 +39,10 @@ const main = async () => {
     waveCount = await waveContract.getTotalWaves();
     allWaves = await waveContract.getAllWaves();
     console.log("AllWaves \n", allWaves);
+
+    // Read new balance
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance));
 }
 
 const runMain = async () => {
